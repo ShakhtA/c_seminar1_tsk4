70ead57f713ac7b54ecec4e14bf16fbeff2daccd
Andrey <shakht_a@mail.ru>
2023-02-24 11:18:48 +0300

    задача 4 нахождение максимального из трех чисел

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Ввод трех целых чисел
+
+Console.WriteLine("Введите три целых числа");
+
+string Number = Console.ReadLine() ?? "";
+int a = int.Parse(Number);
+
+Number = Console.ReadLine() ?? "";
+int b = int.Parse(Number);
+
+Number = Console.ReadLine() ?? "";
+int c = int.Parse(Number);
+
+//Нахождение максимального числа
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+//Вывод результата
+
+Console.WriteLine($"{a}, {b}, {c} -> {max}");
